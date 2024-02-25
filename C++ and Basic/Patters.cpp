a6a8e333e2ffb2b6094e598d8fcb96fa4abf0b57
Priyanshu-Jha <priyanshuchotu1@gmail.com>
2024-02-25 16:10:25 +0530

    Learning Patterns

---
+++ C++ and Basic/Patters.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// 1. Square
+void printSquare(int sizeOfSide){
+    for(int i = 0;i<sizeOfSide;i++){
+        for(int j = 0;j<sizeOfSide;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
+
+
+int main(){
+    int sizeOfSide;
+    cin>>sizeOfSide;
+    //printSquare(sizeOfSide);
+}
