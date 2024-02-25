3ff374e82d459597f840ed16e9e6fed8ace7cb54
Priyanshu-Jha <priyanshuchotu1@gmail.com>
2024-02-25 16:23:24 +0530

    Learning Pattern

--- C++ and Basic/Patters.cpp
+++ C++ and Basic/Patters.cpp
@@ -11,10 +11,29 @@ void printSquare(int sizeOfSide){
     }
 }
 
+// 2. Hollow Rectangle
+void printHollowRectangle(int row,int column){
+    for(int i = 0;i<row;i++){
+        for(int j = 0;j<column;j++){
+            if(i == 0 or i == row-1){
+                cout<<"* ";
+            }
+            else if(j == 0 or j == column-1)
+                cout<<"* ";
+            else
+                cout<<"  ";
+        }
+        cout<<endl;
+    }
+}
 
+// 3.  
 
 int main(){
     int sizeOfSide;
-    cin>>sizeOfSide;
+    int row,column;
+    cin>>row>>column;
+    printHollowRectangle(row,column);
+    //cin>>sizeOfSide;
     //printSquare(sizeOfSide);
 }
