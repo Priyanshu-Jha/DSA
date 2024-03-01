5d5343a9c7beb34fe09f940142c6aeae542fafe9
Priyanshu-Jha <priyanshuchotu1@gmail.com>
2024-03-01 17:31:07 +0530

    function learning

--- C++ and Basic/tempCodeRunnerFile.cpp
+++ C++ and Basic/tempCodeRunnerFile.cpp
@@ -1,10 +1,3 @@
-for(int i = 0;i<sizePym;i++){
-        for(int j = 0;j<sizePym-i-1;j++)
-            cout<<" ";
-        for(int j = 0;j<=i;j++)
-            if(j == 0 or j == i)
-                cout<<"* ";
-            else
-                cout<<"  ";
-        cout<<endl;
-    }
+void main(){
+    cout<<"Hello1";
+}
