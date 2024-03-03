c16f0f85e9cd47be2bcc7704c3a9fa5633647919
Priyanshu-Jha <priyanshuchotu1@gmail.com>
2024-03-03 18:47:17 +0530

    comiit function

--- C++ and Basic/func.cpp
+++ C++ and Basic/func.cpp
@@ -1,5 +1,16 @@
 #include<iostream>
 using namespace std;
-int main(int x){
-    cout<<"Hello";
+void checkPrimeNumber(int n){
+    for(int i = 2;i*i<=n;i++){
+        if(n%i == 0)
+            return;
+    }
+    cout<<n<<endl;
+}
+int main(){
+    int n,sum = 0;
+    cin>>n;
+    for(int i = 2;i<n+1;i++)
+        checkPrimeNumber(i);
+    return  0;
 }
